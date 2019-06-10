606ba2e089398722b1b31f743458ca656e68906e
Anant gupta <anantgupta@gmail.com>
2019-06-10 04:04:54 -0500

    stock app view extension (class based views)

--- apple/apps/stock/views.py
+++ apple/apps/stock/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import TemplateView ,ListView, DetailView
-from apps.stock.models import Stock
+from apps.stock.models import Stock, Analysis
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from utils import CurrentDateTime
+import pandas_datareader.data as web
 
 
 class StockListView(ListView):
@@ -23,14 +25,32 @@ class StockDetailView(DetailView):
 	template_name = 'stock/stock_detail.html'
 
 	def get_context_data(self, **kwargs):
-		print("this is what you get in life")
-		print(kwargs)
 		# Call the base implementation first to get a context.
 		context = super().get_context_data(**kwargs)
-		print("this is the context")
+		start = CurrentDateTime().week_ago_datetime
+		end = CurrentDateTime().today
+		stock_info = web.DataReader(context['stock'].ticker, 'yahoo', start, end)
+		s_high, s_low, s_open, _, s_vol, s_price = stock_info.iloc[stock_info.index.argmax()]
+		context['stock_info'] = {
+			'high': s_high,
+			'low': s_low,
+			'close': s_price,
+			'volume': s_vol,
+			'open': s_open
+		}
+		context['notes'] = Analysis.objects.filter(ticker=kwargs['object'])
+		return context
+
+
+
+class StockNotesView(ListView):
+	model = Analysis
+	template_name = 'stock/stock_notes.html'
+	context_object_name = 'notes_object'
+
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context.
+		context = super().get_context_data(**kwargs)
+		context['var'] = 'Bullish'
 		print(context)
-		stock = get_object_or_404(Stock, **kwargs)
-		print("this is the stock")
-		print(stock)
-		print(kwargs)
 		return context
