91a2706868548ca582bfb793e69dadc763bb334e
Anant gupta <anantgupta@gmail.com>
2019-06-10 04:03:16 -0500

    Models: Revised; added new fields

--- apple/apps/stock/models.py
+++ apple/apps/stock/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apps.stock.behaviors import Nameable, Stockable, Timestampable
 
+from django.urls import reverse
 
 class Stock(Nameable, Timestampable):
 	ticker = models.CharField(max_length=5)
@@ -8,14 +9,41 @@ class Stock(Nameable, Timestampable):
 	def __str__(self):
 		return "Share: {}[{}]".format(self.name, self.ticker)
 
+	def get_absolute_url(self):
+		return reverse("stock:detail", kwargs={"pk": self.id})
+
+	def get_list_url(self):
+		return reverse("stock:List")
+
 class Analysis(Stockable, Timestampable):
+	BEAR = 'Bearish'
+	BULL = 'Bullish'
+	SIDEWAYS = 'sideways'
+	stock_stance = [
+		(BEAR, 'Bearish'),
+		(BULL, 'Bullish'),
+		(SIDEWAYS, 'Neutral')
+	]
+
+	priority_trade = [
+		(1, 'Highly Interested'),
+		(2, 'Interested'),
+		(3, 'Wait'),
+		(4, 'Risk')
+	]
+
 	resistance = models.DecimalField(max_digits=7, decimal_places=2)
 	support = models.DecimalField(max_digits=7, decimal_places=2)
 	stock_range = models.CharField(max_length=50)
 	comments = models.CharField(max_length=1000, blank=True, null=True)
+	stance = models.CharField(max_length=10, choices=stock_stance, default=SIDEWAYS)
+	priority = models.IntegerField(choices=priority_trade, default=3)
 
 	def __str__(self):
 		return "Analysis Component: {} [{}] ".format(self.ticker, self.stock_range)
+
+	def get_absolute_url(self):
+		return reverse("stock:notes")
 
 class Market(Stockable, Timestampable):
 	open = models.DecimalField(max_digits=7, decimal_places=2)
